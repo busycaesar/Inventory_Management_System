4a46eb44ec91a2c757d42b43cf03bf0ca8d40a46
Dev Shah <busycaesar@gmail.com>
2024-03-22 13:17:47 -0400

    Init new JavaFX project

---
+++ src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// Create a BorderPane and load the fxml file.
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+			
+			// Create a scene, load the BorderPane, and set the application dimensions.
+			Scene scene = new Scene(root,860,580);
+			
+			// Set the scene on the stage.
+			primaryStage.setScene(scene);
+			
+			// Set the property of the stage.
+			primaryStage.setTitle("Inventory Management System");
+			primaryStage.setResizable(false);
+			
+			// Display the stage.
+			primaryStage.show();
+		} catch(Exception e) { e.printStackTrace(); }
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module Inventory_Management_System {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
