8764809af032068a6c4fbf83a2987fef84be8adb
Dev Shah <busycaesar@gmail.com>
2024-03-25 12:17:48 -0400

    Add(Model/Product): Create the constructor of Products class

--- src/Model/Product.java
+++ src/Model/Product.java
@@ -16,4 +16,24 @@ public class Product {
 	// All the parts associated with the product.
 	ArrayList<Part> associatedParts = new ArrayList<>();
 	
+	// Load the product object from the database using id.
+	public Product(int _id) {
+	}
+	
+	// Create a new product object.
+	public Product(int _stock, int _minRequire, int _maxCapacity, String _name, double _price, ArrayList<Part> _associatedParts) {
+		this.stock = _stock;
+		this.minRequire = _minRequire;
+		this.maxCapacity = _maxCapacity;
+		this.name = _name;
+		this.price = _price;
+		this.associatedParts = _associatedParts;
+		this.id = this.generateProductId();
+	}
+	
+	// Generate a new unique id.
+	private int generateProductId() {
+		return 0;
+	}
+	
 }
