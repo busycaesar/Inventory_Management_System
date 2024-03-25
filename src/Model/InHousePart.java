c51a7d9c11c888f608a96a6d9967a30e38a8fc1f
Dev Shah <busycaesar@gmail.com>
2024-03-25 13:49:36 -0400

    Add(Model): Create getters and setter for Inhousepart and Outsourcepart

--- src/Model/InHousePart.java
+++ src/Model/InHousePart.java
@@ -7,7 +7,7 @@ public class InHousePart extends Part {
 	// Create a new part object.
 	public InHousePart(int _id, int _stock, int _minRequire, int _maxCapacity, String _name, double _price, int _machineId) {
 		super(_id, _stock, _minRequire, _maxCapacity, _name, _price);
-		this.machineId = _machineId;
+		this.setMachineId(_machineId);
 	}
 	
 	public void setMachineId(int _machineId) {
--- src/Model/OutsourcePart.java
+++ src/Model/OutsourcePart.java
@@ -4,8 +4,9 @@ public class OutsourcePart extends Part {
 
 	private String companyName;
 	
-	public OutsourcePart(int _id, int _stock, int _minRequire, int _maxCapacity, String _name, double _price) {
+	public OutsourcePart(int _id, int _stock, int _minRequire, int _maxCapacity, String _name, double _price, String _companyName) {
 		super(_id, _stock, _minRequire, _maxCapacity, _name, _price);	
+		this.setCompanyName(_companyName);
 	}
 
 	public void setCompanyName(String _companyName) {
