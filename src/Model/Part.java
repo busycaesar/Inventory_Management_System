8469cab03047a7bc8ac75a27c14c7e11843b5b27
Dev Shah <busycaesar@gmail.com>
2024-03-25 12:20:42 -0400

    Add(Model/Part): Create the constructor of Part class

--- src/Model/Part.java
+++ src/Model/Part.java
@@ -1,5 +1,36 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Part {
 
+	int    id, 
+		   // The number of parts in stock.
+		   stock, 
+		   // The minimum number of parts required.
+		   minRequire, 
+		   // The maximum number of parts that can be stored.
+		   maxCapacity;
+	String name;
+	double price;
+	
+	// Load the product object from the database using id.
+	public Part(int _id) {
+	}
+	
+	// Create a new product object.
+	public Part(int _stock, int _minRequire, int _maxCapacity, String _name, double _price, ArrayList<Part> _associatedParts) {
+		this.stock = _stock;
+		this.minRequire = _minRequire;
+		this.maxCapacity = _maxCapacity;
+		this.name = _name;
+		this.price = _price;
+		this.id = this.generatePartId();
+	}
+	
+	// Generate a new unique id.
+	private int generatePartId() {
+		return 0;
+	}
+	
 }
--- src/Model/Product.java
+++ src/Model/Product.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class Product {
 	
-	int id, 
-		// The number of product in stock.
-		stock, 
-		// The minimum number of products required.
-		minRequire, 
-		// The maximum number of products that can be stored.
-		maxCapacity;
+	int    id, 
+		   // The number of product in stock.
+		   stock, 
+		   // The minimum number of products required.
+		   minRequire, 
+		   // The maximum number of products that can be stored.
+		   maxCapacity;
 	String name;
 	double price;
 	// All the parts associated with the product.
